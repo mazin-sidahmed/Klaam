8dcf3fa40d45c87192b1ffc8e87f47c5509893a2
Faris Mohammed <farismosman@gmail.com>
2017-09-10 21:14:36 +0200

    removed un needed exception

--- app/src/main/java/com/example/mazin/klaam/MainActivity.java
+++ app/src/main/java/com/example/mazin/klaam/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQ_CODE_SPEECH_INPUT = 100;
     private TextView mVoiceInputTv;
     private ImageButton mSpeakBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,17 +31,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     private void startVoiceInput() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ar-SA");
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ar-SA");
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "العربية");
-        try {
-            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-        } catch (ActivityNotFoundException a) {
-
-        }
+        startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
